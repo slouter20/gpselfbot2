f600f049f44b53301678be77f21cbdbce9e1ad13
slouter20 <41645499+slouter20@users.noreply.github.com>
2018-07-30 19:57:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // -->COMMANDS<--
 
 bot.on('guildMemberAdd',member =>{
-    member.send('**:fire::rose: Selam Güzel İnsan :rose::fire:**\n **:fire: Sıcak Bir Aile Ortamının Vede Eğlencenin Olduğu Bir Sunucuya Ne Dersiniz.:fire:**\n **:rose: Oyun, Sohbet, Eğlence, Müziğin Olduğu Her Şey Bu Sunucuda :rose:**\n **:fire: Bizi Diğerlerinden Farklı Kılan Şey Korunmanız. Hiç Bir Şekilde Saldırı Gerçekleştirilemeyen Bir Sunucu Yarattık. :fire:**\n **:rose: 7/24 Açık Yetkililerimiz Vardır. Siz Farkında Olmasanız Bile Birileri Sizleri İzliyor Olacak :rose:**\n **:fire: Seni De Bu Ortamda Görmekten Mutluluk Duyarız. :fire:**\n **:rose: Sunucu Davet Linki; https://discord.gg/6ZgPARX :rose:**\n ');
+    member.send('**:fire::rose: Selam Güzel İnsan :rose::fire:**\n **:fire: Sıcak Bir Aile Ortamının Vede Eğlencenin Olduğu Bir Sunucuya Ne Dersiniz.:fire:**\n **:rose: Oyun, Sohbet, Eğlence, Müziğin Olduğu Her Şey Bu Sunucuda :rose:**\n **:fire: Bizi Diğerlerinden Farklı Kılan Şey Korunmanız. Hiç Bir Şekilde Saldırı Gerçekleştirilemeyen Bir Sunucu Yarattık. :fire:**\n **:rose: 7/24 Açık Yetkililerimiz Vardır. Siz Farkında Olmasanız Bile Birileri Sizleri İzliyor Olacak :rose:**\n **:fire: Seni De Bu Ortamda Görmekten Mutluluk Duyarız. :fire:**\n **:rose: Sunucu Davet Linki; https://discord.gg/6ZgPARX :rose:**\n https://cdn.discordapp.com/attachments/466345989297537025/473186049376518186/GangstasParadise.gif\n ');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
 
@@ -14,7 +14,7 @@ bot.on('guildMemberAdd',member =>{
 
 
 bot.on('guildMemberRemove', member=>{
-    member.send('**:fire::rose: Selam Güzel İnsan :rose::fire:**\n **:fire: Sıcak Bir Aile Ortamının Vede Eğlencenin Olduğu Bir Sunucuya Ne Dersiniz.:fire:**\n **:rose: Oyun, Sohbet, Eğlence, Müziğin Olduğu Her Şey Bu Sunucuda :rose:**\n **:fire: Bizi Diğerlerinden Farklı Kılan Şey Korunmanız. Hiç Bir Şekilde Saldırı Gerçekleştirilemeyen Bir Sunucu Yarattık. :fire:**\n **:rose: 7/24 Açık Yetkililerimiz Vardır. Siz Farkında Olmasanız Bile Birileri Sizleri İzliyor Olacak :rose:**\n **:fire: Seni De Bu Ortamda Görmekten Mutluluk Duyarız. :fire:**\n **:rose: Sunucu Davet Linki; https://discord.gg/6ZgPARX :rose:**\n ');
+    member.send('**:fire::rose: Selam Güzel İnsan :rose::fire:**\n **:fire: Sıcak Bir Aile Ortamının Vede Eğlencenin Olduğu Bir Sunucuya Ne Dersiniz.:fire:**\n **:rose: Oyun, Sohbet, Eğlence, Müziğin Olduğu Her Şey Bu Sunucuda :rose:**\n **:fire: Bizi Diğerlerinden Farklı Kılan Şey Korunmanız. Hiç Bir Şekilde Saldırı Gerçekleştirilemeyen Bir Sunucu Yarattık. :fire:**\n **:rose: 7/24 Açık Yetkililerimiz Vardır. Siz Farkında Olmasanız Bile Birileri Sizleri İzliyor Olacak :rose:**\n **:fire: Seni De Bu Ortamda Görmekten Mutluluk Duyarız. :fire:**\n **:rose: Sunucu Davet Linki; https://discord.gg/6ZgPARX :rose:**\n https://cdn.discordapp.com/attachments/466345989297537025/473186049376518186/GangstasParadise.gif\n ');
 
     //member.guild.defaultChannel.sendmessage(`Welcome "${member.user.username}"! Be sure to set your platform by typing "!role"`);
 
